c0e68fffc597608010df107f17f571fd9e8c53d3
Dolma Gurung <dolmagurung147@gmail.com>
2019-06-28 00:27:38 -0400

    basic signup skeleton

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
 
 class Signup extends Component {
+
+  state = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    dob: '',
+    sexualRepresentation: ''
+  }
+
+  onChangeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onClickHandler = (e) => {
+    console.log(this.state)
+  }
+
   render(){
     return(
       <div>
+        First Name: <input type='text' value={this.state.firstName} name='firstName' placeholder='First Name' onChange={this.onChangeHandler} />
+        Last Name: <input type='text' value={this.state.lastName} name='lastName' placeholder='Last Name' onChange={this.onChangeHandler} />
+        Username: <input type='text' value={this.state.username} name='username' placeholder='Username' onChange={this.onChangeHandler} />
+        Password: <input type='password' value={this.state.password} name='password' placeholder='Password' onChange={this.onChangeHandler} />
+        Date of Birth: <input type='date' value={this.state.dob} name='dob' placeholder='Date of Birth' onChange={this.onChangeHandler} />
+        Sexual Represenatation: <select onChange={this.onChangeHandler} value={this.state.sexualRepresentation} name='sexualRepresentation'>
+          <option>please select</option>
+          <option>she/her</option>
+          <option>he/his</option>
+          <option>they/their</option>
+        </select>
+        <button onClick={this.onClickHandler}>Sign Up</button>
       </div>
     )
   }
 }
+
+export default Signup
--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Login from '../Components/Login'
+import Signup from '../Components/Signup'
 
 class MainContainer extends Component {
   render() {
@@ -8,6 +9,7 @@ class MainContainer extends Component {
       <div>
         <h1>Hello World!</h1>
         <Login />
+        <Signup />
       </div>
     )
   }
